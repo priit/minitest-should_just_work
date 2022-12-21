c495499731132002467cea047063878306ae3bd7
Priit Tark <priit@domify.io>
2022-12-21 14:50:43 +0200

    init

---
+++ lib/minitest/should_just_work.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "should_just_work/version"
+
+module Minitest
+  module ShouldJustWork
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
